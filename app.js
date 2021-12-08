362ca617fd301c0f6989dd2fc4db47ea89a58abc
Valentyn <xdshkaa2@gmail.com>
2021-12-08 14:30:00 +0200

    master: fixed bugs

--- app.js
+++ app.js
@@ -49,21 +49,15 @@ const start = async () => {
 
   const filesNames = fs.readdirSync(settings.pathToTigerSettingsFolder);
 
-  let doesExistSettingsFile = false;
-  let pathToSettingsFile = settings.pathToTigerSettingsFolder;
+  let pathsToSettingsFiles = [];
 
   filesNames.forEach(fileName => {
-    if (doesExistSettingsFile) {
-      return true;
-    }
-
     if (fileName.includes('Charting_')) {
-      doesExistSettingsFile = true;
-      pathToSettingsFile += `/${fileName}`;
+      pathsToSettingsFiles.push(`${settings.pathToTigerSettingsFolder}/${fileName}`);
     }
   });
 
-  if (!doesExistSettingsFile) {
+  if (!pathsToSettingsFiles.length) {
     console.log('Не могу найти файл конфигурации в папке');
     return false;
   }
@@ -95,107 +89,109 @@ const start = async () => {
     workAmounts.push(Math.floor(depositForCalculate * i));
   }
 
-  const fileContent = fs.readFileSync(pathToSettingsFile, 'utf8');
-  const parsedContent = await xml2js.parseStringPromise(fileContent);
+  for await (const pathToSettingsFile of pathsToSettingsFiles) {
+    const fileContent = fs.readFileSync(pathToSettingsFile, 'utf8');
+    const parsedContent = await xml2js.parseStringPromise(fileContent);
 
-  const workingSymbols = [];
+    const workingSymbols = [];
 
-  const size1Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size1Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-  const size2Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size2Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-  const size3Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size3Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-  const size4Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size4Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-  const size5Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size5Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
+    const size1Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size1Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
+    const size2Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size2Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
+    const size3Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size3Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
+    const size4Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size4Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
+    const size5Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size5Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
 
-  size1Arr.forEach(e => {
-    const symbolName = e['d5p1:Key'][0].split('_')[0];
-    workingSymbols.push(symbolName);
-  });
+    size1Arr.forEach(e => {
+      const symbolName = e['d5p1:Key'][0].split('_')[0];
+      workingSymbols.push(symbolName);
+    });
 
-  if (!workingSymbols.length) {
-    console.log('В файле конфигурации нет инструментов');
-    return false;
-  }
-
-  const firstValues = [];
-
-  workingSymbols.forEach((workingSymbolName, indexSymbol) => {
-    const exchangeInfoSymbol = exchangeInfo.symbols.find(
-      symbol => symbol.symbol === workingSymbolName,
-    );
-
-    if (!exchangeInfoSymbol) {
-      console.log(`Не могу найти совпадение; symbol: ${workingSymbolName}`);
-      return true;
+    if (!workingSymbols.length) {
+      console.log(`В файле конфигурации нет инструментов; path: ${pathToSettingsFile}`);
+      return false;
     }
 
-    if (!exchangeInfoSymbol.filters || !exchangeInfoSymbol.filters.length || !exchangeInfoSymbol.filters[2].stepSize) {
-      console.log(`Не могу найти stepSize; symbol: ${workingSymbolName}`);
-      return null;
-    }
+    const firstValues = [];
 
-    const instrumentPriceDoc =  instrumentsPrices.find(doc => doc.symbol === workingSymbolName);
+    workingSymbols.forEach((workingSymbolName, indexSymbol) => {
+      const exchangeInfoSymbol = exchangeInfo.symbols.find(
+        symbol => symbol.symbol === workingSymbolName,
+      );
 
-    if (!instrumentPriceDoc) {
-      console.log(`Не могу найти цену; symbol: ${workingSymbolName}`);
-      return null;
-    }
+      if (!exchangeInfoSymbol) {
+        console.log(`Не могу найти совпадение; symbol: ${workingSymbolName}`);
+        return true;
+      }
 
-    const stepSize = parseFloat(exchangeInfoSymbol.filters[2].stepSize);
-    const instrumentPrice = parseFloat(instrumentPriceDoc.price);
-    const stepSizePrecision = getPrecision(stepSize);
+      if (!exchangeInfoSymbol.filters || !exchangeInfoSymbol.filters.length || !exchangeInfoSymbol.filters[2].stepSize) {
+        console.log(`Не могу найти stepSize; symbol: ${workingSymbolName}`);
+        return null;
+      }
 
-    const result = workAmounts.map(workAmount => {
-      let tmp = workAmount / instrumentPrice;
+      const instrumentPriceDoc =  instrumentsPrices.find(doc => doc.symbol === workingSymbolName);
 
-      if (tmp < stepSize) {
-        tmp = stepSize;
-      } else {
-        const remainder = tmp % stepSize;
+      if (!instrumentPriceDoc) {
+        console.log(`Не могу найти цену; symbol: ${workingSymbolName}`);
+        return null;
+      }
 
-        if (remainder !== 0) {
-          tmp -= remainder;
+      const stepSize = parseFloat(exchangeInfoSymbol.filters[2].stepSize);
+      const instrumentPrice = parseFloat(instrumentPriceDoc.price);
+      const stepSizePrecision = getPrecision(stepSize);
 
-          if (tmp < stepSize) {
-            tmp = stepSize;
+      const result = workAmounts.map(workAmount => {
+        let tmp = workAmount / instrumentPrice;
+
+        if (tmp < stepSize) {
+          tmp = stepSize;
+        } else {
+          const remainder = tmp % stepSize;
+
+          if (remainder !== 0) {
+            tmp -= remainder;
+
+            if (tmp < stepSize) {
+              tmp = stepSize;
+            }
           }
         }
-      }
 
-      if (!Number.isInteger(tmp)) {
-        tmp = tmp.toFixed(stepSizePrecision);
-      }
-
-      return parseFloat(tmp, 10);
-    });
-
-    [size1Arr, size2Arr, size3Arr, size4Arr, size5Arr].forEach((arr, index) => {
-      const value = result[index];
-
-      if (indexSymbol === 0) {
-        firstValues.push(value);
-      }
-
-      arr.forEach(e => {
-        const symbolName = e['d5p1:Key'][0].split('_')[0];
-
-        if (symbolName !== workingSymbolName) {
-          return true;
+        if (!Number.isInteger(tmp)) {
+          tmp = tmp.toFixed(stepSizePrecision);
         }
 
-        e['d5p1:Value'][0] = value;
+        return parseFloat(tmp, 10);
+      });
+
+      [size1Arr, size2Arr, size3Arr, size4Arr, size5Arr].forEach((arr, index) => {
+        const value = result[index];
+
+        if (indexSymbol === 0) {
+          firstValues.push(value);
+        }
+
+        arr.forEach(e => {
+          const symbolName = e['d5p1:Key'][0].split('_')[0];
+
+          if (symbolName !== workingSymbolName) {
+            return true;
+          }
+
+          e['d5p1:Value'][0] = value;
+        });
       });
     });
-  });
 
-  parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size1Param'][0]['d4p1:Value'][0] = 1;
-  parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size2Param'][0]['d4p1:Value'][0] = 2;
-  parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size3Param'][0]['d4p1:Value'][0] = 3;
-  parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size4Param'][0]['d4p1:Value'][0] = 4;
-  parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size5Param'][0]['d4p1:Value'][0] = 5;
+    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size1Param'][0]['d4p1:Value'][0] = 1;
+    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size2Param'][0]['d4p1:Value'][0] = 2;
+    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size3Param'][0]['d4p1:Value'][0] = 3;
+    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size4Param'][0]['d4p1:Value'][0] = 4;
+    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size5Param'][0]['d4p1:Value'][0] = 5;
 
-  const builder = new xml2js.Builder();
-  const xml = builder.buildObject(parsedContent);
-  fs.writeFileSync(pathToSettingsFile, xml);
+    const builder = new xml2js.Builder();
+    const xml = builder.buildObject(parsedContent);
+    fs.writeFileSync(pathToSettingsFile, xml);
+  }
 
   console.log('Process was finished');
 };
