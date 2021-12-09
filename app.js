8ddf8758cc45625784084e7ecb14599f46650745
Valentyn <xdshkaa2@gmail.com>
2021-12-09 16:43:51 +0200

    master: started clicker

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
-const path = require('path');
+const robot = require('robotjs');
+const ncp = require('copy-paste');
 const readline = require('readline');
 const { execSync } = require('child_process');
 
 let settings = {
   areModulesLoaded: false,
-  pathToTigerSettingsFolder: false,
 };
 
 const updateSettings = () => {
@@ -25,8 +25,6 @@ if (!settings.areModulesLoaded) {
   updateSettings();
 }
 
-const xml2js = require('xml2js');
-
 const {
   getExchangeInfo,
 } = require('./binance/get-exchange-info');
@@ -40,37 +38,40 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+let isReady = false;
+let xAndYOfVolumePanel = false;
 let depositForCalculate = false;
 
+const orderSteps = [
+  'depositForCalculate',
+  'xAndYOfVolumePanel',
+];
+
+const currentStep = {
+  index: 0,
+  stepName: orderSteps[0],
+
+  incrementStep() {
+    this.index += 1;
+    this.stepName = orderSteps[this.index];
+  },
+};
+
 const start = async () => {
-  if (!settings.pathToTigerSettingsFolder) {
-    return askQuestion('whereTigerSettingsFolder');
-  }
-
-  const filesNames = fs.readdirSync(settings.pathToTigerSettingsFolder);
-
-  let pathsToTradingSettingsFiles = [];
-  let pathsToChartingSettingsFiles = [];
-
-  filesNames.forEach(fileName => {
-    if (fileName.includes('Charting_')) {
-      pathsToChartingSettingsFiles.push(`${settings.pathToTigerSettingsFolder}/${fileName}`);
-    }
-
-    if (fileName.includes('Trading_')) {
-      pathsToTradingSettingsFiles.push(`${settings.pathToTigerSettingsFolder}/${fileName}`);
-    }
-  });
-
-  if (!pathsToChartingSettingsFiles.length && !pathsToTradingSettingsFiles.length) {
-    console.log('Не могу найти файл конфигурации в папке');
-    return false;
-  }
-
   if (!depositForCalculate) {
     return askQuestion('depositForCalculate');
   }
 
+  if (!xAndYOfVolumePanel) {
+    console.log('Нажмите мышкой на место, где находится панель объемов');
+    return true;
+  }
+
+  // if (!isReady) {
+  //   return askQuestion('areYouReady');
+  // }
+
+  /*
   const resultGetExchangeInfo = await getExchangeInfo();
 
   if (!resultGetExchangeInfo || !resultGetExchangeInfo.status) {
@@ -87,6 +88,7 @@ const start = async () => {
 
   const exchangeInfo = resultGetExchangeInfo.result;
   const instrumentsPrices = resultGetInstrumentsPrices.result;
+  */
 
   const workAmounts = [];
 
@@ -94,245 +96,52 @@ const start = async () => {
     workAmounts.push(Math.floor(depositForCalculate * i));
   }
 
-  // Trading
-  for await (const pathToSettingsFile of pathsToTradingSettingsFiles) {
-    const fileContent = fs.readFileSync(pathToSettingsFile, 'utf8');
-    const parsedContent = await xml2js.parseStringPromise(fileContent);
-
-    const workingSymbols = [];
-
-    const size1Arr = parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset1'][0]['d3p1:SizeParam'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-    const size2Arr = parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset2'][0]['d3p1:SizeParam'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-    const size3Arr = parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset3'][0]['d3p1:SizeParam'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-    const size4Arr = parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset4'][0]['d3p1:SizeParam'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-    const size5Arr = parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset5'][0]['d3p1:SizeParam'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-
-    size1Arr.forEach(e => {
-      const symbolName = e['d5p1:Key'][0].split('_')[0];
-      workingSymbols.push(symbolName);
-    });
-
-    if (!workingSymbols.length) {
-      console.log(`Trading: В файле конфигурации нет инструментов; path: ${pathToSettingsFile}`);
-      continue;
-    }
-
-    workingSymbols.forEach(workingSymbolName => {
-      const exchangeInfoSymbol = exchangeInfo.symbols.find(
-        symbol => symbol.symbol === workingSymbolName,
-      );
-
-      if (!exchangeInfoSymbol) {
-        console.log(`Trading: Не могу найти совпадение; symbol: ${workingSymbolName}`);
-        return true;
-      }
-
-      if (!exchangeInfoSymbol.filters || !exchangeInfoSymbol.filters.length || !exchangeInfoSymbol.filters[2].stepSize) {
-        console.log(`Trading: Не могу найти stepSize; symbol: ${workingSymbolName}`);
-        return true;
-      }
-
-      const instrumentPriceDoc =  instrumentsPrices.find(doc => doc.symbol === workingSymbolName);
-
-      if (!instrumentPriceDoc) {
-        console.log(`Trading: Не могу найти цену; symbol: ${workingSymbolName}`);
-        return true;
-      }
-
-      const stepSize = parseFloat(exchangeInfoSymbol.filters[2].stepSize);
-      const instrumentPrice = parseFloat(instrumentPriceDoc.price);
-      const stepSizePrecision = getPrecision(stepSize);
-
-      const result = workAmounts.map(workAmount => {
-        let tmp = workAmount / instrumentPrice;
-
-        if (tmp < stepSize) {
-          tmp = stepSize;
-        } else {
-          const remainder = tmp % stepSize;
-
-          if (remainder !== 0) {
-            tmp -= remainder;
-
-            if (tmp < stepSize) {
-              tmp = stepSize;
-            }
-          }
-        }
-
-        if (!Number.isInteger(tmp)) {
-          tmp = tmp.toFixed(stepSizePrecision);
-        }
-
-        return parseFloat(tmp, 10);
-      });
-
-      [size1Arr, size2Arr, size3Arr, size4Arr, size5Arr].forEach((arr, index) => {
-        const value = result[index];
-
-        arr.forEach(e => {
-          const symbolName = e['d5p1:Key'][0].split('_')[0];
-
-          if (symbolName !== workingSymbolName) {
-            return true;
-          }
-
-          e['d5p1:Value'][0] = value;
-        });
-      });
-    });
-
-    parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset1'][0]['d3p1:SizeParam'][0]['d4p1:Value'][0] = 1;
-    parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset2'][0]['d3p1:SizeParam'][0]['d4p1:Value'][0] = 2;
-    parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset3'][0]['d3p1:SizeParam'][0]['d4p1:Value'][0] = 3;
-    parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset4'][0]['d3p1:SizeParam'][0]['d4p1:Value'][0] = 4;
-    parsedContent.TradingSettings.MarketSettings[0]['d2p1:Preset5'][0]['d3p1:SizeParam'][0]['d4p1:Value'][0] = 5;
-
-    const builder = new xml2js.Builder();
-    const xml = builder.buildObject(parsedContent);
-    fs.writeFileSync(pathToSettingsFile, xml);
-  }
-
-  // Charting
-  for await (const pathToSettingsFile of pathsToChartingSettingsFiles) {
-    const fileContent = fs.readFileSync(pathToSettingsFile, 'utf8');
-    const parsedContent = await xml2js.parseStringPromise(fileContent);
-
-    const workingSymbols = [];
-
-    const size1Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size1Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-    const size2Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size2Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-    const size3Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size3Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-    const size4Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size4Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-    const size5Arr = parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size5Param'][0]['d4p1:Values'][0]['d5p1:KeyValueOfstringdouble'];
-
-    size1Arr.forEach(e => {
-      const symbolName = e['d5p1:Key'][0].split('_')[0];
-      workingSymbols.push(symbolName);
-    });
-
-    if (!workingSymbols.length) {
-      console.log(`Charting: В файле конфигурации нет инструментов; path: ${pathToSettingsFile}`);
-      continue;
-    }
-
-    workingSymbols.forEach(workingSymbolName => {
-      const exchangeInfoSymbol = exchangeInfo.symbols.find(
-        symbol => symbol.symbol === workingSymbolName,
-      );
-
-      if (!exchangeInfoSymbol) {
-        console.log(`Charting: Не могу найти совпадение; symbol: ${workingSymbolName}`);
-        return true;
-      }
-
-      if (!exchangeInfoSymbol.filters || !exchangeInfoSymbol.filters.length || !exchangeInfoSymbol.filters[2].stepSize) {
-        console.log(`Charting: Не могу найти stepSize; symbol: ${workingSymbolName}`);
-        return true;
-      }
-
-      const instrumentPriceDoc =  instrumentsPrices.find(doc => doc.symbol === workingSymbolName);
-
-      if (!instrumentPriceDoc) {
-        console.log(`Charting: Не могу найти цену; symbol: ${workingSymbolName}`);
-        return true;
-      }
-
-      const stepSize = parseFloat(exchangeInfoSymbol.filters[2].stepSize);
-      const instrumentPrice = parseFloat(instrumentPriceDoc.price);
-      const stepSizePrecision = getPrecision(stepSize);
-
-      const result = workAmounts.map(workAmount => {
-        let tmp = workAmount / instrumentPrice;
-
-        if (tmp < stepSize) {
-          tmp = stepSize;
-        } else {
-          const remainder = tmp % stepSize;
-
-          if (remainder !== 0) {
-            tmp -= remainder;
-
-            if (tmp < stepSize) {
-              tmp = stepSize;
-            }
-          }
-        }
-
-        if (!Number.isInteger(tmp)) {
-          tmp = tmp.toFixed(stepSizePrecision);
-        }
-
-        return parseFloat(tmp, 10);
-      });
-
-      [size1Arr, size2Arr, size3Arr, size4Arr, size5Arr].forEach((arr, index) => {
-        const value = result[index];
-
-        arr.forEach(e => {
-          const symbolName = e['d5p1:Key'][0].split('_')[0];
-
-          if (symbolName !== workingSymbolName) {
-            return true;
-          }
-
-          e['d5p1:Value'][0] = value;
-        });
-      });
-    });
-
-    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size1Param'][0]['d4p1:Value'][0] = 1;
-    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size2Param'][0]['d4p1:Value'][0] = 2;
-    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size3Param'][0]['d4p1:Value'][0] = 3;
-    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size4Param'][0]['d4p1:Value'][0] = 4;
-    parsedContent.ChartingSettings.ChartSettings[0]['d2p1:TradeSettings'][0]['d2p1:Size5Param'][0]['d4p1:Value'][0] = 5;
-
-    const builder = new xml2js.Builder();
-    const xml = builder.buildObject(parsedContent);
-    fs.writeFileSync(pathToSettingsFile, xml);
-  }
-
   console.log('Process was finished');
 };
 
 const askQuestion = (nameStep) => {
-  if (nameStep === 'whereTigerSettingsFolder') {
-    rl.question('Укажите полный путь к файлу настроек tiger\n', userAnswer => {
-      if (!userAnswer) {
-        console.log('Вы ничего не ввели');
-        return askQuestion('whereTigerSettingsFolder');
-      }
+  switch (nameStep) {
+    case 'areYouReady': {
+      rl.question('Все готово? (yes)\n', userAnswer => {
+        if (!userAnswer) {
+          console.log('Вы ничего не ввели');
+          return askQuestion('areYouReady');
+        }
 
-      if (!fs.existsSync(userAnswer)) {
-        console.log('Не нашел папку');
-        return askQuestion('whereTigerSettingsFolder');
-      }
+        if (userAnswer !== 'yes') {
+          return askQuestion('areYouReady');
+        }
 
-      settings.pathToTigerSettingsFolder = userAnswer;
-      updateSettings();
+        isReady = true;
+        return start();
+      });
 
-      return start();
-    });
-  }
+      break;
+    }
 
-  if (nameStep === 'depositForCalculate') {
-    rl.question('Введите ваш депозит\n', userAnswer => {
-      if (!userAnswer) {
-        console.log('Вы ничего не ввели');
-        return askQuestion('depositForCalculate');
-      }
-
-      if (!userAnswer
-        || Number.isNaN(parseFloat(userAnswer))
-        || userAnswer < 0) {
-          console.log('Невалидные данные');
+    case 'depositForCalculate': {
+      rl.question('Введите ваш депозит\n', userAnswer => {
+        if (!userAnswer) {
+          console.log('Вы ничего не ввели');
           return askQuestion('depositForCalculate');
-      }
+        }
 
-      depositForCalculate = parseFloat(userAnswer);
-      return start();
-    });
+        if (!userAnswer
+          || Number.isNaN(parseFloat(userAnswer))
+          || userAnswer < 0) {
+            console.log('Невалидные данные');
+            return askQuestion('depositForCalculate');
+        }
+
+        depositForCalculate = parseFloat(userAnswer);
+        currentStep.incrementStep();
+        return start();
+      });
+
+      break;
+    }
   }
+
 };
 
 const getPrecision = (price) => {
@@ -345,4 +154,33 @@ const getPrecision = (price) => {
   return dividedPrice[1].length;
 };
 
+// ioHook.on('mouseclick', (event) => {
+//   const {
+//     x, y,
+//   } = event;
+//
+//   console.log('x', x, 'y', y);
+//
+//   switch (currentStep.stepName) {
+//     case 'xAndYOfVolumePanel': {
+//       xAndYOfVolumePanel = [x, y];
+//       currentStep.incrementStep();
+//       start();
+//       break;
+//     }
+//
+//     default: break;
+//   }
+// });
+
 start();
+
+/*
+setTimeout(() => {
+  for (let i = 0; i < 10; i += 1) {
+    robot.keyTap('down');
+    robot.keyTap('c', ['command']);
+    console.log(ncp.paste());
+  }
+}, 5000);
+*/
--- robot.js
+++ robot.js
@@ -1,21 +1,14 @@
 const ioHook = require('iohook');
+const robot = require('robotjs');
 
 const start = () => {
   ioHook.on('mouseclick', (event) => {
-    console.log('here', event); // { type: 'mousemove', x: 700, y: 400 }
+    console.log('here', event);
   });
 
   ioHook.start();
 
-  const id = ioHook.registerShortcut(
-    [29, 65],
-    (keys) => {
-      console.log('Shortcut called with keys:', keys);
-    },
-    (keys) => {
-      console.log('Shortcut has been released!');
-    }
-  );
+
 };
 
 start();
